14d265f72c75b00f57b9559cf09e87cfa553638b
jduarte <jduartelic@gmail.com>
2020-07-24 19:32:11 -0400

    feat(jd): add configs

---
+++ fronten-app-list-value-currencys/next.config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const withSass = require('@zeit/next-sass');
+
+module.exports = withSass({
+  webpack: (config, { defaultLoaders }) => {
+    return config;
+  },
+  poweredByHeader: false,
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+  },
+});
