1f84ee50df7ca1e10b5b1b0460beea723d6876c0
jduarte <jduartelic@gmail.com>
2020-07-24 19:34:55 -0400

    feat(jd): add HomeLanding component and style

---
+++ fronten-app-list-value-currencys/src/components/Home/index.js
@@ -0,0 +1,14 @@
+class Home extends React.Component {
+  render() {
+    return (
+      <section className="home-landing__container">
+        <h1 className="home-landing__title">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <div className="home-landing__container-grid">PRODUCT BOXES</div>
+      </section>
+    );
+  }
+}
+export default Home;
