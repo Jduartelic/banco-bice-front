25b2428e587d205a5a3509fa36e9abc85f80e6fa
jduarte <jduartelic@gmail.com>
2020-07-26 20:58:03 -0400

    chore(jd): remove not need it code

--- fronten-app-list-value-currencys/utils/constants.js
+++ fronten-app-list-value-currencys/utils/constants.js
@@ -1,20 +1,6 @@
 import getConfig from 'next/config';
-import { format } from 'currencyformatter.js';
 
-const { publicRuntimeConfig } = getConfig();
 const { publicRuntimeConfig = {} } = getConfig() || {};
-
-const parameters = {
-  currency: 'CLP', // If currency is not supplied, defaults to USD
-  symbol: '$', // Overrides the currency's default symbol
-  locale: 'cl', // Overrides the currency's default locale (see supported locales)
-  decimal: ',', // Overrides the locale's decimal character
-  group: '.', // Overrides the locale's group character (thousand separator)
-  pattern: '!#.##0,00', // Overrides the locale's default display pattern
-
-  // The pattern follows standard unicode currency pattern notation.
-  // comma = group separator, dot = decimal separator, exclamation = currency symbol
-};
 
 export const InfoCurrencys = {
   dolar: { symbol: '$', currency: 'USD' },
