ad2277bacdbc1102a1111de11a31af769d8c9724
jduarte <jduartelic@gmail.com>
2020-07-27 12:27:41 -0400

    fix(jd): remove unused old files

--- fronten-app-list-value-currencys/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  setupFiles: ['./jest.setup.js'],
-  testPathIgnorePatterns: ['./.next/', './node_modules/'],
-  moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': 'babel-jest',
-  },
-};
--- fronten-app-list-value-currencys/redux/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import CommodityReducer from '../modules/commodity/commodity.state';
-
-const reducers = {
-  commodity: CommodityReducer,
-};
-
-export default combineReducers(reducers);
