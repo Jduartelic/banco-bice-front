7e0f590c1e8506226fc09f605692390b3637fb8f
jduarte <jduartelic@gmail.com>
2020-08-12 10:54:30 -0400

    feat(jd): refactor title app

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import NavBar from '@components/NavBar';
-import PropTypes from 'prop-types';
+import { handleResize } from '@utils/windowBreakpoints';
 import './Layout.scss';
 
-const Layout = props => {
-  const { children } = props;
-  return (
-    <main className="layout__wrapper">
-      <NavBar />
-      <div className="layout__wrapper-container">{children}</div>
-      {/* TODO ADD FOOTER */}
-    </main>
-  );
-};
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: false,
+    };
+  }
+  componentDidMount() {
+    this.handleWindowSizeChange();
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    handleResize(this.state, newState => this.setState(newState), window);
+  };
+
+  render() {
+    const { children } = this.props;
+    const propsNavBar = { isMobile: this.state.isMobile };
+    return (
+      <main className="layout__wrapper">
+        <NavBar {...propsNavBar} />
+        <div className="layout__wrapper-container">{children}</div>
+        {/* TODO ADD FOOTER */}
+      </main>
+    );
+  }
+}
 
 export default Layout;
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import IconHamburguer from '@components/Common/Icons/icon-hamburguer';
 import Router from 'next/router';
+import PropTypes from 'prop-types';
 
 import './NavBar.scss';
 
-const NavBar = () => {
+const NavBar = props => {
+  const title = props.isMobile ? 'Stock Market' : "Stock Market Index's";
   return (
     <div className="nav-bar__container">
       <div
@@ -16,12 +18,16 @@ const NavBar = () => {
         <div className="nav-bar__hamburguer">
           <IconHamburguer style={{ width: '3.5rem', height: '100%' }} />
         </div>
-        <div className="nav-bar__img-container">
-          <img className="nav-bar__img" src={'../../../static/logo-bice.png'} />
+        <div className="nav-bar__title-container">
+          <h1 className="nav-bar__title">{title}</h1>
         </div>
       </div>
     </div>
   );
+};
+
+NavBar.propTypes = {
+  isMobile: PropTypes.bool.isRequired,
 };
 
 export default NavBar;
